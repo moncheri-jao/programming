905e2c8d08e4e798417a895cd04722881382e7a8
birrabenzina <chemistryisforlosers@gmail.com>
2022-01-17 21:58:14 +0100

    corrected compile.plot.sh

--- c/derivative/compile.plot.sh
+++ c/derivative/compile.plot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cc -Wall -pedantic -lm -o derivative.exe derivative.c	# compile the file using the system's c compiler
-./der.exe						# execute the generated executable and generate the file catched by the python scripts
-python plotter.py &					# execute the python script in background, leave only the plotted graph
+cc -Wall -pedantic -lm -o derivative.exe derivative.c # compile the file using the system's c compiler
+./derivative.exe                                      # execute the generated executable and generate the file catched by the python scripts
+python plotter.py &# execute the python script in background, leave only the plotted graph
