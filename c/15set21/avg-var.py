20650f1090f896a6ebcad9a01ce137da65ed5ef0
birrabenzina <chemistryisforlosers@gmail.com>
2022-02-10 18:20:45 +0100

    adding c++, perl, php folders

--- c/15set21/avg-var.py
+++ c/15set21/avg-var.py
@@ -1,5 +1,5 @@
-import matplotlib as m
+import matplotlib.pyplot as m
 import numpy as n
 
 passi, avg, var = n.loadtxt("stat.dat", unpack=True)
-y = n.linspace(0,passi[-1]
+y = n.linspace(0,passi[-1])
