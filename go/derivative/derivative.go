83ba392f65ca600ce80153777c63f6248803e622
birrabenzina <chemistryisforlosers@gmail.com>
2022-02-14 19:13:57 +0100

    translated derivative.c in go

--- go/derivative/derivative.go
+++ go/derivative/derivative.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"fmt"
-	"math" 
+	"math"
+	"os"
 )
 
 func f (x float64) float64 {
 	return (math.Sqrt(x));
 }
 
-func derivative (f func (float64) float64, x float64, eps float64) float64 {
+func derivative ( f func (x float64) float64 , x float64, eps float64) float64 {
 	return( (f(x+eps) - f(x))/eps );
 }
 
@@ -17,12 +18,19 @@ func main () {
 	var eps float64 = 1e-10;
 	var rng float64 = 1;
 	var x float64 = 1;
+	stream, err := os.Create("derivative.dat");
+	if err != nil {
+		fmt.Println("ERROR! Cannot create file");
+		panic(err);
+	}
 	fmt.Println("Insert a maximum value for x {[1,x)}");
 	fmt.Scan(&rng);
 	fmt.Println("x \t\t f'(x)");
 	fmt.Println("----------------------------");
 	for x < rng {
 		fmt.Println(x,"|\t|",derivative(f, x, eps));
-		x += 1;
+		fmt.Fprintln(stream,derivative(f,x,eps)," ",x);
+		x += 0.1;
 	}
+	fmt.Println("----------------------------");
 }
